0207ee3352d1745ebd0109c05b6d937054d3ef64
Pedro L <pedro28oliveira.lucas@gmail.com>
2025-01-20 18:38:30 -0300

    A

--- 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/01_heranca_simples.py
@@ -21,15 +21,19 @@ class Carro(Veiculo):
 
 class Caminhao(Veiculo):
     def __init__(self, cor, placa, numero_rodas, carregado):
+        # serve para sobreesrever as caracteristicas da class pai
         super().__init__(cor, placa, numero_rodas)
+        # especifico para classe caminhão, se carro tentar chamar essa funçao, dara errado
         self.carregado = carregado
 
     def esta_carregado(self):
+        # confere por bool se esta ou nao carregado
         print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
 
 
 moto = Motocicleta("preta", "abc-1234", 2)
 carro = Carro("branco", "xde-0098", 4)
+# true or false, para saber se esta carregado
 caminhao = Caminhao("roxo", "gfd-8712", 8, True)
 
 print(moto)
--- 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
@@ -18,6 +18,14 @@ class Ave(Animal):
         super().__init__(**kw)
 
 
+'''
+O IDE junto do python se perdem, pois em um tem 2 argumentos, e no outro tbm. Porem so e passado
+3 argumentos, quando deveriam ser 4. Com isso se usa Kwargs, assim todo argumento novo na classe base
+vai ser passado super().__init__(**kw). Resumindo, tudo apos o **Kw vai ser opcional. Mas tem de ser
+passado de forma nomeada nro_patas=nro_patas.
+'''
+
+
 class Gato(Mamifero):
     pass
 
@@ -27,8 +35,18 @@ class Ornitorrinco(Mamifero, Ave):
         super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
 
 
+'''
+A super()função é usada para dar acesso a métodos e propriedades de uma classe pai ou irmã.
+A super()função retorna um objeto que representa a classe pai.
+'''
 gato = Gato(nro_patas=4, cor_pelo="Preto")
 print(gato)
 
-ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
+ornitorrinco = Ornitorrinco(
+    nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
 print(ornitorrinco)
+
+'''
+__mro__ busca a forma que o interpretador vai fazer a resolução, a ordem etc. Muito bom para
+esse tipo de problemas. "Classe".mro() tbm funciona
+'''
