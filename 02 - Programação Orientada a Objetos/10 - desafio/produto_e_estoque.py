b67ebc40573e06613bb73ee28607b205e586edd7
Pedro L <pedro28oliveira.lucas@gmail.com>
2025-02-03 20:49:35 -0300

    A

--- 02 - Programação Orientada a Objetos/10 - desafio/produto_e_estoque.py
+++ 02 - Programação Orientada a Objetos/10 - desafio/produto_e_estoque.py
@@ -1,30 +1,38 @@
-'''
-Crie uma classe Produto que representa um item em um estoque:
-Atributos: nome (string), quantidade (int).
-Métodos:
-adicionar_estoque(qtd): aumenta a quantidade em estoque.
-remover_estoque(qtd): diminui a quantidade, mas não permite valores negativos.
-exibir_estoque(): retorna "Produto: X, Quantidade: Y".
-'''
+# Crie uma classe Produto que representa um item em um estoque:
+# Atributos: nome (string), quantidade (int).
+# Métodos:
+# adicionar_estoque(qtd): aumenta a quantidade em estoque.
+# remover_estoque(qtd): diminui a quantidade, mas não permite valores negativos.
+# exibir_estoque(): retorna "Produto: X, Quantidade: Y".
 
 
 class Produto:
     def __init__(self, nome, quantidade):
-        # TODO: Inicialize os atributos
+        self.nome = nome
+        self.quantidade = quantidade
+        # Inicialize os atributos
 
     def adicionar_estoque(self, qtd):
-        # TODO: Adicione ao estoque
+        self.quantidade += qtd
+        # Adicione ao estoque
 
     def remover_estoque(self, qtd):
-        # TODO: Remova do estoque com validação
+        if qtd <= self.quantidade:
+            self.quantidade -= qtd
 
     def exibir_estoque(self):
-        # TODO: Retorne as informações formatadas
+        return f"Produto: {self.nome} / Quantidade: {self.quantidade}"
 
-        # Entrada
+
+# Entrada
 nome_produto = input()
 quantidade_inicial = int(input())
 qtd_adicionar = int(input())
 qtd_remover = int(input())
 
-# TODO: Crie a instância e teste os métodos
+produto = Produto(nome_produto, quantidade_inicial)
+
+produto.adicionar_estoque(qtd_adicionar)
+produto.remover_estoque(qtd_remover)
+
+print(produto.exibir_estoque())
