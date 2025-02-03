2b4ec428a15e960787af353562f6a1e748b6e373
Pedro L <pedro28oliveira.lucas@gmail.com>
2025-02-03 20:17:42 -0300

    hjgfgh

--- 02 - Programação Orientada a Objetos/10 - desafio/aluno_media.py
+++ 02 - Programação Orientada a Objetos/10 - desafio/aluno_media.py
@@ -6,20 +6,50 @@ adicionar_nota(nota): adiciona uma nota à lista.
 calcular_media(): retorna a média das notas.
 '''
 
+# class Aluno:
+#     def __init__(self, nome):
+#         #  Inicialize os atributos
+
+#     def adicionar_nota(self, nota):
+
+#         #  Adicione a nota à lista
+
+#     def calcular_media(self):
+#         #  Calcule a média
+
+#         # Entrada
+# nome_aluno = input()
+# nota1 = float(input())
+# nota2 = float(input())
+
+#  Crie a instância, adicione notas e exiba a média
+
 
 class Aluno:
     def __init__(self, nome):
-        # TODO: Inicialize os atributos
+        self.nome = nome  # Atributo para armazenar o nome do aluno
+        self.notas = []   # Lista vazia para armazenar as notas
 
     def adicionar_nota(self, nota):
-        # TODO: Adicione a nota à lista
+        self.notas.append(nota)  # Adiciona uma nova nota à lista
 
     def calcular_media(self):
-        # TODO: Calcule a média
+        # Calcula a média somando todas as notas e dividindo pela quantidade
+        return sum(self.notas) / len(self.notas)
 
-        # Entrada
+
+# Entrada do usuário
 nome_aluno = input()
 nota1 = float(input())
 nota2 = float(input())
 
-# TODO: Crie a instância, adicione notas e exiba a média
+# Cria uma instância do Aluno
+aluno = Aluno(nome_aluno)
+
+# Adiciona as notas ao objeto aluno
+aluno.adicionar_nota(nota1)
+aluno.adicionar_nota(nota2)
+
+# Calcula e imprime a média
+media = aluno.calcular_media()
+print(media)
--- 02 - Programação Orientada a Objetos/10 - desafio/calculos_geometricos.py
+++ 02 - Programação Orientada a Objetos/10 - desafio/calculos_geometricos.py
@@ -9,16 +9,23 @@ calcular_perimetro(): retorna 2*(largura + altura).
 
 class Retangulo:
     def __init__(self, largura, altura):
+        self.altura = altura
+        self.largura = largura
         # TODO: Inicialize os atributos
 
     def calcular_area(self):
-        # TODO: Implemente a área
+        return self.largura * self.altura  # ja q tem o INIT nao precisa definir so puxar
 
     def calcular_perimetro(self):
-        # TODO: Implemente o perímetro
+        # ja q tem o INIT nao precisa definir so puxar
+        return 2*(self.largura + self.altura)
 
         # Entrada
 largura = float(input())
-altura = float(input())
-
-# TODO: Crie a instância e exiba área e perímetro
+altura = float(input())  # até essa parte eu msm fiz
+#  Crie a instância e exiba área e perímetro
+# nicializa o objeto com as dimensões fornecidas.
+retangulo = Retangulo(largura, altura)
+# Calcula e imprime os resultados
+print(retangulo.calcular_area())
+print(retangulo.calcular_perimetro())
