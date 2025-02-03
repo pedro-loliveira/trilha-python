cad80ab6eb9c3d9c4b3396dd45c05865add089a6
Pedro L <pedro28oliveira.lucas@gmail.com>
2025-02-03 13:41:13 -0300

    trampei

--- 03 - Decoradores, Iteradores e Geradores/2_funcao_interna.py
+++ 03 - Decoradores, Iteradores e Geradores/2_funcao_interna.py
@@ -8,7 +8,12 @@ def principal():
         print("executando a funcao 2")
 
     funcao_interna()
-    funcao_2()
+    funcao_2()  # so da pra chamar a função secundaria dentro da primaria, assim
+    # quando a primaria for chamada, chama a secundaria, mas se a secundaria for
+    # chamada fora da primaria, dara erro
+
+# funcao_interna() #erro
+# funcao_2() #erro
 
 
 principal()
